1a446ddeeec252290b64a6d18fdfd5feb3734c9d
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-03-22 16:20:02 +0000

    created Employee class

--- src/main/java/Staff/Employee.java
+++ src/main/java/Staff/Employee.java
@@ -1,4 +1,40 @@
-package Staff;
+package staff;
 
-public class Employee {
+public abstract class Employee {
+
+    private String name;
+    private String niNumber;
+    private Double salary;
+
+    public Employee(String name, String niNumber, Double salary) {
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNiNumber() {
+        return niNumber;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void raiseSalary(Double salaryIncrease){
+        setSalary(this.salary + salaryIncrease);
+    }
+
+    public double payBonus(){
+        return this.salary * 0.01;
+    }
+
 }
